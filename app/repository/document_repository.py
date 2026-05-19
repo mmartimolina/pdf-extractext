09483e1671fca1cc1eb1adfccd7ce582900a4277
Martina <martinaabrilmolina912@gmail.com>
2026-05-19 12:53:19 -0300

    Agregar auditoria y soft delete. Closes #18 Closes #19

--- app/repository/document_repository.py
+++ app/repository/document_repository.py
@@ -6,6 +6,7 @@ import os
 from pymongo import MongoClient
 from bson import ObjectId
 from pymongo.errors import DuplicateKeyError
+from datetime import datetime
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/pdf_db")
 
@@ -16,10 +17,20 @@ db = client["pdf_db"]
 
 # Colección
 collection = db["documents"]
+collection.create_index("checksum", unique=True)  # Asegurar que el checksum sea único  
 
 
 # CREATE
 def guardar_documento(documento):
+
+    #Auditoria
+    documento["created_at"] = datetime.utcnow()
+    documento["updated_at"] = datetime.utcnow() 
+    
+    #Soft delete
+    documento["deleted"] = False    
+    documento["deleted_at"] = None
+
     try:
         return collection.insert_one(documento)
     except DuplicateKeyError:
@@ -27,12 +38,17 @@ def guardar_documento(documento):
 
 # READ (por checksum)
 def obtener_por_checksum(checksum: str):
-    return collection.find_one({"checksum": checksum})
+    return collection.find_one({
+        "checksum": checksum,
+        "deleted": False  # Solo documentos no eliminados
+    })
 
 
 # READ (todos)
 def obtener_todos():
-    documentos = list(collection.find())
+    documentos = list(collection.find({
+        "deleted": False  # Solo documentos no eliminados
+    }))
 
     # Convertir ObjectId a string para evitar errores JSON
     for doc in documentos:
@@ -55,20 +71,31 @@ def actualizar_documento(checksum: str, nuevo_nombre=None, nuevo_texto=None):
     if not update_fields:
         return None
 
+    #Auditoria
+    update_fields["updated_at"] = datetime.utcnow()  # Actualizar timestamp de actualización
     resultado = collection.update_one(
         {"checksum": checksum},
+        {"deleted": False},  # Asegurar que el documento no esté marcado como eliminado
         {"$set": update_fields}
     )
 
     if resultado.matched_count == 0:
         return None
 
-    return collection.find_one({"checksum": checksum})
+    return collection.find_one({
+        "checksum": checksum,
+        "deleted": False  # Solo documentos no eliminados
+    })
 
 
 # DELETE (por checksum)
 def eliminar_documento(checksum: str):
-
-    resultado = collection.delete_one({"checksum": checksum})
-
-    return resultado.deleted_count > 0
+    resultado = collection.update_one(
+        {"checksum": checksum,
+         "deleted":False},
+        {"$set": {
+            "deleted": True,
+            "deleted_at": datetime.utcnow()  # Marcar fecha de eliminación
+        }}
+    )   
+    return resultado.modified_count > 0     
